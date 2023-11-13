c4e4d02164b172a3dac7dee98c2a0d3e41867c61
harmony <harmony.dgt@gmail.com>
2023-11-13 16:40:32 -0300

    '..'

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -3,15 +3,30 @@
 
  
 
-  function validation(event,guser){ 
+  function validation(event,guser,gphoto){ 
     
       event.preventDefault() 
       
       var gname=guser
+      var gnphoto=gphoto
       validateView.classList.toggle('show')
       console.log('gname',gname)
       console.log('user',document.getElementById('user'))
-  
+
+      var userContainer=document.getElementById('user')
+
+      userContainer.innerHTML=`
+
+      
+      <div>
+        `+gname+` 
+      </div>
+      
+        <div>
+          `+gnphoto+` 
+        </div>
+      
+      `
 
   }
 
